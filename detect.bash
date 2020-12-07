26524f96c1a2c0fa390aa3a9382d9d1363f95678
connorlee77 <connorlee77@gmail.com>
2020-12-06 23:34:28 -0800

    detect.py

---
+++ detect.bash
@@ -0,0 +1,27 @@
+python detect.py --source=kitti/original --output=kitti_cams/original --coco_index=26
+python detect.py --source=kitti/original --output=kitti_cams/original --coco_index=9
+python detect.py --source=kitti/original --output=kitti_cams/original --coco_index=7
+python detect.py --source=kitti/original --output=kitti_cams/original --coco_index=5
+python detect.py --source=kitti/original --output=kitti_cams/original --coco_index=2
+python detect.py --source=kitti/original --output=kitti_cams/original --coco_index=0
+
+python detect.py --source=kitti/4k --output=kitti_cams/4k --coco_index=26
+python detect.py --source=kitti/4k --output=kitti_cams/4k --coco_index=9
+python detect.py --source=kitti/4k --output=kitti_cams/4k --coco_index=7
+python detect.py --source=kitti/4k --output=kitti_cams/4k --coco_index=5
+python detect.py --source=kitti/4k --output=kitti_cams/4k --coco_index=2
+python detect.py --source=kitti/4k --output=kitti_cams/4k --coco_index=0
+
+python detect.py --source=kitti/7k --output=kitti_cams/7k --coco_index=26
+python detect.py --source=kitti/7k --output=kitti_cams/7k --coco_index=9
+python detect.py --source=kitti/7k --output=kitti_cams/7k --coco_index=7
+python detect.py --source=kitti/7k --output=kitti_cams/7k --coco_index=5
+python detect.py --source=kitti/7k --output=kitti_cams/7k --coco_index=2
+python detect.py --source=kitti/7k --output=kitti_cams/7k --coco_index=0
+
+python detect.py --source=kitti/10k --output=kitti_cams/10k --coco_index=26
+python detect.py --source=kitti/10k --output=kitti_cams/10k --coco_index=9
+python detect.py --source=kitti/10k --output=kitti_cams/10k --coco_index=7
+python detect.py --source=kitti/10k --output=kitti_cams/10k --coco_index=5
+python detect.py --source=kitti/10k --output=kitti_cams/10k --coco_index=2
+python detect.py --source=kitti/10k --output=kitti_cams/10k --coco_index=0
